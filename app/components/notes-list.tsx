4edc8e62f70ddcf2605556a96c394d97109a9f82
soichiro0915 <soichiro.ex.eru0915@gmail.com>
2022-12-27 01:23:47 +0900

    data fetch in server component SSG, SSR, ISR

---
+++ app/components/notes-list.tsx
@@ -0,0 +1,43 @@
+import type { Database } from '../../database.types'
+import { format } from 'date-fns'
+
+type Note = Database['public']['Tables']['notes']['Row']
+
+async function fetchNotes() {
+  // await new Promise((resolve) => setTimeout(resolve, 2000)) // 不要
+  const res = await fetch(`${process.env.url}/rest/v1/notes?select=*`, {
+    headers: new Headers({
+      apikey: process.env.apikey as string,
+    }),
+    // cache: 'no-store', //SSG 表記なし or 'force-cache'  SSR 'no-cache'
+    next: { revalidate: 10 }, //ISR
+  })
+  if (!res.ok) {
+    throw new Error('Failed to fetch data in server')
+  }
+  const notes: Note[] = await res.json()
+  return notes
+}
+
+export default async function NotesList() {
+  const notes = await fetchNotes()
+
+  return (
+    <div>
+      <p className="my-4 pb-3 text-xl font-medium underline underline-offset-4">
+        Notes
+      </p>
+      <ul className="m-3">
+        {notes.map((note) => (
+          <li key={note.id}>
+            <p> {note.title}</p>
+            <p>
+              <strong className="mr-3">Created at:</strong>
+              {note && format(new Date(note.created_at), 'yyyy-MM-dd HH:mm:ss')}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import NotesList from './components/notes-list'
+
 export default function page() {
   return (
     <main>
-      <div className="m-10 text-center">Hello World</div>
+      <div className="m-10 text-center">
+        <p>Hello World</p>
+        {/* @ts-ignore*/}
+        <NotesList />
+      </div>
     </main>
   )
 }
