5f6a0f66b19564e65b6a6a33ed488b44006355f2
soichiro0915 <soichiro.ex.eru0915@gmail.com>
2022-12-27 10:53:30 +0900

    server component, client componentの比較実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
+import { Suspense } from 'react'
+
 import NotesList from './components/notes-list'
+import TimerCounter from './components/timer-counter'
+import Spinner from './components/spinner'
+import RefreshBtn from './components/refresh-btn'
 
 export default function page() {
   return (
     <main>
       <div className="m-10 text-center">
         <p>Hello World</p>
-        {/* @ts-ignore*/}
-        <NotesList />
+        <Suspense fallback={<Spinner color="border-green-500" />}>
+          {/* @ts-ignore*/}
+          <NotesList />
+        </Suspense>
+        <TimerCounter />
+        <RefreshBtn />
       </div>
     </main>
   )
