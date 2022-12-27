71e4bf6f4ffa698e560138b5625867c947df7483
soichiro0915 <soichiro.ex.eru0915@gmail.com>
2022-12-27 12:46:04 +0900

    supabase suth in server component

---
+++ app/auth/layout.tsx
@@ -0,0 +1,26 @@
+import { headers, cookies } from 'next/headers'
+import SupabaseListener from '../components/supabase-listener'
+import { createServerComponentSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import type { Database } from '../../database.types'
+
+export default async function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  // client側の認証情報をサーバーサイドで使用できるようにする
+  const supabase = createServerComponentSupabaseClient<Database>({
+    headers,
+    cookies,
+  })
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  return (
+    <>
+      <SupabaseListener accessToken={session?.access_token} />
+      {children}
+    </>
+  )
+}
